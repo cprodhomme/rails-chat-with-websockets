4153241834653e025a9dcebcbe78ebd3e0d2a60d
Cle61 <7428736+cle61@users.noreply.github.com>
2019-04-13 00:17:58 +0200

    Add websockets and more

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -1,3 +1,5 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(function() {
+  $('#new_room_message').on('ajax:success', function(a, b,c ) {
+    $(this).find('input[type="text"]').val('');
+  });
+});
